b411766959c2c1b80d387b2c4c05df899b507e58
ryunosukearima <ryunosuke.arima125@gmail.com>
2025-06-20 22:56:48 +0900

    ヘッダーコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-import Music from '../src/components/music/Music'
+import Music from './components/music/Music'
+import Header from './components/header/Header'
+
 function App() {
 
   return (
     <div className='bg-slate-500 w-full h-screen'>
+      <Header />
       <Music />
     </div>
   )
